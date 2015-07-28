fb3bef3a7e6e8ebf389e89c5b46d6e2170e60453
dzhf21@hotmail.com <dzhf21@hotmail.com>
2015-07-28 23:27:16 +0800

    bug fix

--- dynamic_programming/maximalSquare.cpp
+++ dynamic_programming/maximalSquare.cpp
@@ -17,4 +17,32 @@ int maximalSquare(vector<vector<char> > & matrix) {
   if (m==0) return 0;
   int n=matrix[0].size();
   if (n==0) return 0;
+  vector<int> L(n,0);
+  vector<int> R(n,n);
+  vector<int> H(n,0);
+  int r = 0;
+  for(int i = 0; i < m; i++) {
+    int left  = 0, right = n;
+    for(int j = 0; j < n; j++) {
+      if( matrix[i][j] == '1' ) {
+        L[j] = max(L[j],left);
+        H[j]++;
+      } else {
+        L[j] = 0;
+        R[j] = n;
+        H[j] = 0;
+        left = j + 1;
+      }
+    }
+    for(int j = n - 1; j >= 0; j--) {
+      if( matrix[i][j] == '1' ) {
+        R[j] = min(R[j],right);
+        r = max(r, H[j] * ( R[j] - L[j] ));
+      } else {
+        R[j] = n;
+        right = j;
+      }
+    }
+  }
+  return r;
 }
