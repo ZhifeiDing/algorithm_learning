ff513ef7ac1a0ad0f74744033677efa7dabda6f8
dzhf21@hotmail.com <dzhf21@hotmail.com>
2015-08-14 08:32:21 +0800

    remove tree

--- tree/lowestCommonAncestor.cpp
+++
@@ -1,9 +0,0 @@
-#include "leetcode.hpp"
-
-TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-       if (p -> val < root -> val && q -> val < root -> val)
-              return lowestCommonAncestor(root -> left, p, q);
-       if (p -> val > root -> val && q -> val > root -> val)
-              return lowestCommonAncestor(root -> right, p, q);
-       return root;
-}
