9d7e4b48e4428cb30c7a933d4587a729adccaf3e
dzhf21@hotmail.com <dzhf21@hotmail.com>
2015-10-27 08:42:45 +0800

    case update

---
+++ math/singleNumberII.cpp
@@ -0,0 +1,12 @@
+#include "leetcode.hpp"
+
+/*
+ * Given an array of integers, every element appears three times except 
+ * for one. Find that single one.
+ * 
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you 
+ * implement it without using extra memory?
+ */
+int singleNumberII(vector<int> & nums) {
+}
