d4861152bf329e23d2421c185377c8333a97058e
dzhf21@hotmail.com <dzhf21@hotmail.com>
2015-11-23 19:30:41 +0800

    bug fix

--- dynamic_programming/test_NumArray.cpp
+++ dynamic_programming/test_NumArray.cpp
@@ -8,10 +8,10 @@ void test_NumArray() {
   gen_array(n, nums);
   print_array(nums);
   int m = rand()%5+1;
+  NumArray numArray(nums);
   while( --m ) {
     int i = rand()%n;
     int j = i + rand()%(n-i);
-    NumArray numArray(nums);
     cout << "Sum(" << i << " , " << j << ") = " << numArray.sumRange(i,j) << endl;
   }
 }
---
+++ tree_graph/test_NumArray.cpp
@@ -0,0 +1,24 @@
+#include "leetcode.hpp"
+#include "NumArray.cpp"
+
+void test_NumArray() {
+  srand((unsigned int)time(NULL));
+  int n = rand()%12;
+  vector<int> nums;
+  gen_array(n, nums);
+  print_array(nums);
+  int m = rand()%5+1;
+  NumArray numArray(nums);
+  while( --m ) {
+    int i = rand()%n;
+    int j = i + rand()%(n-i);
+    cout << "Sum(" << i << " , " << j << ") = " << numArray.sumRange(i,j) << endl;
+
+    int val = rand()%100;
+    nums[i] = val;
+    numArray.update(i,val);
+    cout << "Modify nums : ";
+    print_array(nums);
+    cout << "Sum(" << i << " , " << j << ") = " << numArray.sumRange(i,j) << endl;
+  }
+}
