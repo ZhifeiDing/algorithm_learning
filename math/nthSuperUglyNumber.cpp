8699fc9ba9417244bb30140f12361214c02bd16c
ZhifeiDing <dzhf21@hotmail.com>
2015-12-06 09:25:41 +0800

    find nth super ugly number with the given primes

---
+++ math/nthSuperUglyNumber.cpp
@@ -0,0 +1,34 @@
+#include "leetcode.hpp"
+
+/*
+ * Write a program to find the nth super ugly number.
+ * 
+ * Super ugly numbers are positive numbers whose all prime factors 
+ * are in the given prime list primes of size k. For example, 
+ * [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32] is the sequence of the 
+ * first 12 super ugly numbers given primes = [2, 7, 13, 19] of size 4.
+ * 
+ * Note:
+ * (1) 1 is a super ugly number for any given primes.
+ * (2) The given numbers in primes are in ascending order.
+ * (3) 0 < k ≤ 100, 0 < n ≤ 106, 0 < primes[i] < 1000.
+ */
+
+int nthSuperUglyNumber(int n,  vector<int> & primes) {
+  set<long> ugly;
+  ugly.insert(1);
+
+  int i = 1;
+  int ret = 1;
+
+  while( i <= n ) {
+    auto itr = ugly.begin();
+    ret = *itr;
+    ugly.erase(itr);
+
+    for(auto i : primes)
+      ugly.insert(ret * i);
+    ++i;
+  }
+  return ret;
+}
---
+++ math/test_nthSuperUglyNumber.cpp
@@ -0,0 +1,32 @@
+#include "leetcode.hpp"
+
+bool isPrime(int n) {
+  for(int i = 2; i <= sqrt(double(n)); ++i)
+    if( n % i == 0 )
+      return false;
+  return true;
+}
+
+void genPrimes(vector<int> &primes, int n) {
+  int i = 0; 
+  int p = 2;
+  while( i < n ) {
+    while( isPrime(p) == false )
+      ++p;
+    primes.push_back(p);
+    ++p;
+    ++i;
+  }
+}
+
+void test_nthSuperUglyNumber() {
+  srand((unsigned int)time(NULL));
+  int n = rand()%20 + 1;
+  vector<int> primes;
+  int m = rand()%10 + 1;
+  genPrimes(primes, m);
+  cout << "primes = ";
+  print_array(primes);
+  int r = nthSuperUglyNumber(n, primes);
+  cout << n << "th ugly number = " << r << endl;
+}
