ee6a6771c6d7d3bf18940b615b0af8a639b17084
ZhifeiDing <dzhf21@hotmail.com>
2016-01-08 22:06:13 +0800

    morris traversal

--- tree_graph/postorderTraversal.cpp
+++ tree_graph/postorderTraversal.cpp
@@ -58,7 +58,58 @@ vector<int> postorderTraversal_iterative(TreeNode* root) {
   return r;
 }
 
+void reverse(TreeNode *from, TreeNode *to) {
+  if( from == to )
+    return;
+  TreeNode *cur = from, *next = from->right;
+
+  while( true ) {
+    TreeNode *t = cur;
+    cur = next;
+    next = next->right;
+    cur->right = t;
+    if( cur == to )
+      break;
+  }
+}
+
+void reverseGet(TreeNode *from, TreeNode *to, vector<int> &r) {
+  reverse(from, to);
+  TreeNode *cur = to;
+  while(true) {
+    r.push_back(cur->val);
+    if( cur == from )
+      break;
+    cur = cur->right;
+  } 
+  reverse(to,from);
+}
+
 vector<int> postorderTraversal_morris(TreeNode* root) {
   vector<int> r;
+  TreeNode dummy(-1);
+  dummy.left = root;
+
+  root = &dummy;
+
+  while( root ) {
+    if( root->left == NULL ) {
+      root = root->right;
+    } else {
+      TreeNode *prev = root->left;
+
+      while( prev->right && prev->right != root )
+        prev = prev->right;
+
+      if( prev->right == NULL ) {
+        prev->right = root;
+        root = root->left;
+      } else {
+        reverseGet(root->left, prev,r);
+        prev->right = NULL;
+        root = root->right;
+      }
+    }
+  }
   return r;
 }
