b07acffc44416a581ab4e6391308d50cd32e6715
ZhifeiDing <dzhf21@hotmail.com>
2016-02-17 19:22:43 +0800

    populate next tree node

---
+++ tree_graph/connectII.cpp
@@ -0,0 +1,47 @@
+#include "leetcode.hpp"
+/*
+ *
+ *Follow up for problem "Populating Next Right Pointers in Each Node".
+ *
+ *What if the given tree could be any binary tree? Would your previous
+ *solution still work?
+ *
+ *Note:
+ *
+ *You may only use constant extra space.
+ *For example,
+ *Given the following binary tree,
+ *         1
+ *       /  \
+ *      2    3
+ *     / \    \
+ *    4   5    7
+ *After calling your function, the tree should look like:
+ *         1 -> NULL
+ *       /  \
+ *      2 -> 3 -> NULL
+ *     / \    \
+ *    4-> 5 -> 7 -> NULL
+ */
+
+void connectII(TreeLinkNode *root) {
+    queue<TreeLinkNode*> q;
+    if( root == NULL )
+        return;
+    q.push(root);
+
+    while( !q.empty() ) {
+        int size = q.size();
+        root = q.front();
+        q.pop();
+        for(int i = 1; i < size; ++i) {
+            root->next = q.front();
+            if( root->left ) q.push(root->left);
+            if( root->right ) q.push(root->right);
+            root = q.front();
+            q.pop();
+        }
+        if( root->left ) q.push(root->left);
+        if( root->right ) q.push(root->right);
+    }
+}
