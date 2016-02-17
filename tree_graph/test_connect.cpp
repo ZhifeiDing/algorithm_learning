62a5d6eaa2134cbb29f61c18e2fbd069c961d1b7
ZhifeiDing <dzhf21@hotmail.com>
2016-02-18 07:55:00 +0800

    populate next tree node

---
+++ tree_graph/test_connect.cpp
@@ -0,0 +1,55 @@
+#include "leetcode.hpp"
+
+void print_tree(TreeLinkNode *root) {
+  vector<vector<int> > r;
+
+  if( root == NULL )
+    return;
+  queue<TreeLinkNode*> q;
+  q.push(root);
+
+  while( !q.empty() ) {
+    int len = q.size();
+    vector<int> level;
+    for(int i = 0; i < len; ++i) {
+      root = q.front();
+      q.pop();
+      level.push_back(root->val);
+      if( root->left != NULL ) q.push(root->left);
+      if( root->right!= NULL ) q.push(root->right);
+    }
+    r.push_back(level);
+  }
+  for(int i = 0; i < r.size(); ++i) {
+    for(auto j : r[i])
+      cout << j << "\t";
+    cout << endl;
+  }
+}
+
+void test_connect() {
+    TreeLinkNode ll3(3);
+    TreeLinkNode lr3(4);
+    TreeLinkNode rl3(5);
+    TreeLinkNode rr3(6);
+
+    TreeLinkNode l2(1, &ll3, &lr3, NULL);
+    TreeLinkNode r2(2, &rl3, &rr3, NULL);
+
+    TreeLinkNode root(0, &l2, &r2, NULL);
+    print_tree(&root);
+    connect(&root);
+
+
+    TreeLinkNode *p = &root;
+    cout << "connect next = " << endl;
+    while( p ) {
+        cout << p->val << " ";
+        while( p->next ) {
+            p = p->next;
+            cout << p->val << " ";
+        }
+        cout << endl;
+        p = p->left;
+    }
+}
